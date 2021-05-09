4fe74e83b0548aa0705650930cb2d0b4800ca959
RakibRahman <md.rr.talukder@gmail.com>
2021-05-10 05:46:22 +0600

    square of every digits of a number

--- square-every-digit.js
+++ square-every-digit.js
@@ -1,4 +1,10 @@
 function squareDigits(num) {
-  return num * num;
+  let str = num.toString();
+  let output = "";
+  for (let i of str) {
+    output += i * i;
+  }
+  return parseInt(output);
 }
-console.log(squareDigits(5));
+console.log(squareDigits(223));
+console.log(squareDigits(9967));
