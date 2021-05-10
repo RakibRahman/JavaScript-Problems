a32df3f0b906768c52d348c4b8d39e6cc5d259a8
RakibRahman <md.rr.talukder@gmail.com>
2021-05-11 02:29:38 +0600

    a program that filters a list of strings and returns a list with only your friends name in it~

--- filter-list-0f-strings.js
+++ filter-list-0f-strings.js
@@ -2,7 +2,7 @@
 /*If a name has exactly 4 letters in it, you can be sure that it has to be a friend of yours! Otherwise, you can be sure he's not... */
 function friend(friends) {
   let output = [];
-  let real = friends.filter((item) => item.length <= 4 && item.length > 3);
+  let real = friends.filter((item) => item.length === 4);
   real.forEach((item) => {
     output.push(item);
   });
