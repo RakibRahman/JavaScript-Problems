3219855e1d060bfb732c62b3272be51513c82e4f
RakibRahman <md.rr.talukder@gmail.com>
2021-05-13 02:46:35 +0600

    Removing nth Array Elements

--- remove-Nth-Elements-From-Array.js
+++ remove-Nth-Elements-From-Array.js
@@ -4,7 +4,7 @@ function removeEveryOther(arr) {
   let i = arr.length;
 
   while (i--) {
-    (i + 1) % 2 === 0 && arr.splice(i, 1); // % 2,%3 is the position for Removing Elements
+    (i + 1) % 2 === 0 && arr.splice(i, 1); // % 2,%3 is the position for Removing Elements and splice modifies the original array
   }
 
   return arr;
@@ -12,3 +12,22 @@ function removeEveryOther(arr) {
 console.log(removeEveryOther([1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
 console.log(removeEveryOther(arr2));
 console.log(removeEveryOther(arr));
+console.log("--------");
+//with for loop
+function removeEveryOther2(arr) {
+  var newArr = [];
+  for (var i = 0; i < arr.length; i += 2) {
+    newArr.push(arr[i]);
+  }
+  return newArr;
+}
+console.log(removeEveryOther2(arr2));
+console.log(removeEveryOther2([5, 6, 5, 6]));
+console.log("--------");
+
+//with filter method
+
+const removeElement = arr.filter((item, n) => {
+  return n % 2 === 0;
+});
+console.log(removeElement);
