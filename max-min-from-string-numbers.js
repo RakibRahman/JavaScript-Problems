6948ab52e3fc9bbe7b0678eba52fb5066da66d3b
RakibRahman <md.rr.talukder@gmail.com>
2021-05-13 00:50:36 +0600

    Highest and Lowest String numbers

--- max-min-from-string-numbers.js
+++ max-min-from-string-numbers.js
@@ -1,3 +1,5 @@
+let str = "1 2 3 4 542 -9";
+
 function highAndLow(numbers) {
   let out = [];
   for (let i of numbers.split(" ")) {
@@ -6,3 +8,10 @@ function highAndLow(numbers) {
   return `${Math.max(...out)} ${Math.min(...out)}`;
 }
 console.log(highAndLow(str));
+
+//clean version
+function highAndLow2(numbers) {
+  numbers = numbers.split(" ");
+  return `${Math.max(...numbers)} ${Math.min(...numbers)}`;
+}
+console.log(highAndLow2(str));
