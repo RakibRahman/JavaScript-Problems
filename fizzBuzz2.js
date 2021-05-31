341c0b81877196a7cf765870f10ed4ea229a424b
RakibRahman <md.rr.talukder@gmail.com>
2021-05-31 22:13:19 +0600

    Another FizzBuzz

--- fizzBuzz2.js
+++ fizzBuzz2.js
@@ -22,3 +22,12 @@ console.log(solution(20));
 // console.log(solution(14));
 // console.log(solution(30));
 // console.log(solution(141));
+
+//! short solution
+function solution(n) {
+  --n;
+  const c15 = Math.floor(n / 15);
+  const c3 = Math.floor(n / 3) - c15;
+  const c5 = Math.floor(n / 5) - c15;
+  return [c3, c5, c15];
+}
