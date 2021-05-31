5beb030037673f36675733e3a2a4e33ad683adb7
RakibRahman <md.rr.talukder@gmail.com>
2021-06-01 01:34:00 +0600

    filter names

---
+++ filterNames.js
@@ -0,0 +1,20 @@
+let arr = [
+  ["foo", "foo@foo.com"],
+  ["bar_", "bar@bar.com"],
+];
+let arr2 = [
+  ["rakib_", "md.rr@gmail.com"],
+  ["zero", "zero"],
+  ["zakir_", "planetzakir"],
+];
+function searchNames(logins) {
+  return logins.filter((x, i) => {
+    if (logins[i][0].slice(-1) === "_") {
+      return x;
+    }
+  });
+  //  return logins.filter(function (login) { return login[0].slice(-1) === '_' });
+}
+
+console.log(searchNames(arr));
+console.log(searchNames(arr2));
