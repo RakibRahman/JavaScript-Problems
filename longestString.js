af8d6d9caa396c16928e81ace5b59e47006a139b
RakibRahman <md.rr.talukder@gmail.com>
2021-06-04 02:30:58 +0600

    find the longest word and its length

--- longestString.js
+++ longestString.js
@@ -9,11 +9,27 @@ function findLongest(str) {
   }
   return longest;
 }
-console.log(findLongest("long string"));
+// console.log(findLongest("long string"));
 
 //! ES6
 function findLongest2(str) {
   var x = str.split(" ");
   return Math.max(...x.map((el) => el.length));
 }
-console.log(findLongest2("long string longer"));
+// console.log(findLongest2("long string longer"));
+
+//get longest string
+function findLongest3(str) {
+  let arr = str.split(" ");
+  let len = 0;
+  let longest;
+  for (let i in arr) {
+    if (arr[i].length > len) {
+      len = arr[i].length;
+      longest = arr[i];
+    }
+  }
+  return longest;
+}
+console.log(findLongest3("killer name is akatsuki"));
+console.log(findLongest("killer name is akatsuki"));
