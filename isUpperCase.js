1c45faca36a4a6df3798de8dc9dbc95c21714a1a
RakibRahman <md.rr.talukder@gmail.com>
2021-06-10 18:27:50 +0600

    check if a string is all uppercase

--- isUpperCase.js
+++ isUpperCase.js
@@ -11,7 +11,18 @@ console.log(rank2.isUpperCase());
 console.log(rank3.isUpperCase());
 console.log("PAIN".isUpperCase());
 
-// short solution
+console.log("-----");
+
+// short solution regex
 const isUpperCase2 = (string) => /^[A-Z]*$/.test(string);
 console.log(isUpperCase2("C"));
 console.log(isUpperCase2("d"));
+
+console.log("-----");
+// short solution 2
+
+const isUpperCase3 = (str) => {
+  return str.toUpperCase() === str;
+};
+console.log(isUpperCase3("RAKIB"));
+console.log(isUpperCase3("RAKiB"));
