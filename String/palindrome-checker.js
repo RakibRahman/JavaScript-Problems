688c63cef141a6dde11a9ed49ed73a62e0956c8d
RakibRahman <md.rr.talukder@gmail.com>
2021-07-20 21:19:15 +0600

    Palindrome Checker

--- String/palindrome-checker.js
+++ String/palindrome-checker.js
@@ -1,1 +1,20 @@
 //!A palindrome is a word, number, phrase, or other sequence of characters which reads the same backward as forward, such as madam or racecar.
+
+function isPalindrome(string) {
+  //   return string.split("").reverse().join("") === string;
+
+  let left = 0;
+  let right = string.length - 1;
+
+  while (left < right) {
+    if (string[left] != string[right]) {
+      return `${false} - ${string} is not a palindrome`;
+    }
+    left++;
+    right--;
+  }
+  return `${true} - ${string} is a palindrome`;
+}
+console.log(isPalindrome("amadam"));
+console.log(isPalindrome("madam"));
+console.log(isPalindrome("racecar"));
