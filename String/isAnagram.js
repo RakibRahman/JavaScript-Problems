a9278f3cc5ff65d782707769b907c4f2abc51293
RakibRahman <md.rr.talukder@gmail.com>
2021-08-01 12:50:15 +0600

    return true if the two arguments given are anagrams of each other

--- String/isAnagram.js
+++ String/isAnagram.js
@@ -18,3 +18,14 @@ const isAnagram = (string1, string2) => {
 console.log(isAnagram("hello", "holle"));
 console.log(isAnagram("kibar", "rakib"));
 console.log(isAnagram("kibaa", "rakib"));
+
+const isAnagram2 = (test, original) => {
+  if (test.length !== original.length) return false;
+  test = test.toLowerCase().split("").sort().join("");
+  original = original.toLowerCase().split("").sort().join("");
+  console.log(test, original);
+  return test === original;
+};
+console.log(isAnagram2("faiza", "zaiaf"));
+console.log(isAnagram2("kasadi", "sadika"));
+console.log(isAnagram2("abcs", "abcd"));
